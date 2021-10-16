34f8fd0fac7ab93de953a9b37be7a04d26dd1bc0
Mahsa <mahsa.alikhany@gmail.com>
2021-10-16 17:06:52 +0330

    added electronicTypes property to Electronic

--- src/model/Electronic.java
+++ src/model/Electronic.java
@@ -1,8 +1,18 @@
 package model;
 
+import model.enumeration.ElectronicTypes;
+
 /**
  * @author Mahsa Alikhani m-58
  */
-public class Electronic {
+public class Electronic extends Product{
+    private ElectronicTypes electronicTypes;
 
+    public ElectronicTypes getElectronicTypes() {
+        return electronicTypes;
+    }
+
+    public void setElectronicTypes(ElectronicTypes electronicTypes) {
+        this.electronicTypes = electronicTypes;
+    }
 }
