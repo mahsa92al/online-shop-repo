4044a9096bc54e6678e6f93185aef8ccdc1d0293
Mahsa <mahsa.alikhany@gmail.com>
2021-10-16 17:54:00 +0330

    created BaseDao

---
+++ src/repository/BaseDao.java
@@ -0,0 +1,24 @@
+package repository;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * @author Mahsa Alikhani m-58
+ */
+public class BaseDao {
+    private static Connection connection;
+
+    public BaseDao() throws SQLException, ClassNotFoundException {
+        connection = getConnection();
+    }
+
+    public static Connection getConnection() throws ClassNotFoundException, SQLException {
+        if(connection == null){
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/online_shop", "root", "5103583");
+        }
+        return connection;
+    }
+}
