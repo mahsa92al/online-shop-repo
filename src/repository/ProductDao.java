498f4c76a0e2f40a120d815bbea64460fc2abce6
Mahsa <mahsa.alikhany@gmail.com>
2021-10-17 23:31:30 +0330

    added findAllProducts to ProductDao

--- src/repository/ProductDao.java
+++ src/repository/ProductDao.java
@@ -1,7 +1,14 @@
 package repository;
 
+import model.Product;
+import model.enumeration.ProductCategory;
+
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Mahsa Alikhani m-58
@@ -12,6 +19,19 @@ public class ProductDao {
     public ProductDao() throws SQLException, ClassNotFoundException {
         this.connection = BaseDao.getConnection();
     }
-
-
+     public List<Product> findAllProducts() throws SQLException {
+        List<Product> products = new ArrayList<>();
+         Statement statement = connection.createStatement();
+         ResultSet resultSet = statement.executeQuery("select * from products");
+         while (resultSet.next()){
+             Product product = new Product();
+             product.setId(resultSet.getInt("id"));
+             product.setName(resultSet.getString("name"));
+             product.setPrice(resultSet.getInt("price"));
+             product.setStock(resultSet.getInt("stock"));
+             product.setCategory(ProductCategory.valueOf(resultSet.getString("category")));
+             products.add(product);
+         }
+         return products;
+     }
 }
