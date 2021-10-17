f49fe6457a88afe666f6a17258cc42a2ade7307b
Mahsa <mahsa.alikhany@gmail.com>
2021-10-17 13:32:42 +0330

    created Customer

---
+++ src/model/Customer.java
@@ -0,0 +1,59 @@
+package model;
+
+/**
+ * @author Mahsa Alikhani m-58
+ */
+public class Customer {
+    private int id;
+    private String name;
+    private String email;
+    private String password;
+    private Address address;
+
+    public Customer(String name, String email, String password, Address address) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.address = address;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+}
