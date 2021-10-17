5a556046738c87023197d4a37a1d6b7539f7dd0b
Mahsa <mahsa.alikhany@gmail.com>
2021-10-17 16:19:02 +0330

    added saveNewAddress to AddressDao

--- src/repository/AddressDao.java
+++ src/repository/AddressDao.java
@@ -1,6 +1,9 @@
 package repository;
 
+import model.Address;
+
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 /**
@@ -11,5 +14,16 @@ public class AddressDao {
 
     public AddressDao() throws SQLException, ClassNotFoundException {
         this.connection = BaseDao.getConnection();
+    }
+
+    public void saveNewAddress(Address address) throws SQLException {
+        String sqlQuery = "insert into address (country, state, city, postal_code)" +
+                "values(?, ?, ?, ?)";
+        PreparedStatement statement = connection.prepareStatement(sqlQuery);
+        statement.setString(1, address.getCountry());
+        statement.setString(2, address.getState());
+        statement.setString(3, address.getCity());
+        statement.setString(4, address.getPostalCode());
+        statement.executeUpdate();
     }
 }
