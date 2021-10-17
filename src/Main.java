1a914a432ad6d3175062e91e7b0afe5a35da0b81
Mahsa <mahsa.alikhany@gmail.com>
2021-10-17 15:32:25 +0330

    added getAddressFromCustomer to Main

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import model.Address;
 import service.CustomerService;
 
 import java.util.Scanner;
@@ -9,6 +10,7 @@ public class Main {
 
     private static final Scanner scanner = new Scanner(System.in);
     private static CustomerService customerService;
+    private static Address address;
 
     public static void main(String[] args) {
         customerService = new CustomerService();
@@ -42,8 +44,13 @@ public class Main {
                 System.out.println(e.getMessage());
             }
         }while (error);
-
-        getAddressFromCustomer();
+        do{
+            try {
+                Address address = getAddressFromCustomer();
+            } catch (Exception e) {
+                System.out.println(e.getMessage());;
+            }
+        }while (error);
 
     }
     private static String getNameFromCustomer() throws Exception {
@@ -69,7 +76,7 @@ public class Main {
         String password = scanner.next();
         return password;
     }
-    private static String getPhoneFromCustomer(){
+    private static String getPhoneFromCustomer() throws Exception {
         System.out.println("Phone");
         String phone = scanner.next();
         if(!phone.matches("^(\\+98|0)?9\\d{9}$")){
@@ -77,6 +84,47 @@ public class Main {
         }
         return phone;
     }
-    private static void getAddressFromCustomer(){}
+    private static Address getAddressFromCustomer() throws Exception {
+        String country = getCountry();
+        String state = getState();
+        String city = getCity();
+        String postalCode = getPostalCode();
+        address = new Address(country, state, city, postalCode);
+        return address;
+    }
+    private static String getCountry() throws Exception {
+        System.out.println("Country:");
+        String country = scanner.next();
+        if(!country.matches("^[a-zA-Z]*$")){
+            throw new Exception("You did not enter letters! please enter only letter.");
+        }
+        return country;
+    }
+    private static String getState() throws Exception {
+        System.out.println("State:");
+        String state = scanner.next();
+        if(!state.matches("^[a-zA-Z]*$")){
+            throw new Exception("You did not enter letters! please enter only letter.");
+        }
+        return state;
+    }
+    private static String getCity() throws Exception {
+        System.out.println("City:");
+        String city = scanner.next();
+        if(!city.matches("^[a-zA-Z]*$")){
+            throw new Exception("You did not enter letters! please enter only letter.");
+        }
+        return city;
+    }
+    private static String getPostalCode() throws Exception {
+        System.out.println("Postal code:");
+        String postalCode = scanner.next();
+        if(!postalCode.matches("[1-9]+")){
+            throw new Exception("You did not enter numbers! please enter only numbers.");
+        }
+        return postalCode;
+    }
+
+
 
 }
