a8fc7291abdab134c6a8c91d7bcd7c507d6da9ee
Mahsa <mahsa.alikhany@gmail.com>
2021-10-17 22:50:50 +0330

    created ProductService

---
+++ src/service/ProductService.java
@@ -0,0 +1,19 @@
+package service;
+
+import model.Product;
+import repository.ProductDao;
+
+import java.sql.SQLException;
+import java.util.List;
+
+/**
+ * @author Mahsa Alikhani m-58
+ */
+public class ProductService {
+    private final ProductDao productDao;
+
+    public ProductService() throws SQLException, ClassNotFoundException {
+        this.productDao = new ProductDao();
+    }
+
+}
