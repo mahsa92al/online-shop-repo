1cf4ac4770fe5edd4d9a0d755cda3108e140c6a4
Mahsa <mahsa.alikhany@gmail.com>
2021-10-17 08:34:10 +0330

    added exception to addNewOrderToList in ElectronicService

--- src/service/ElectronicService.java
+++ src/service/ElectronicService.java
@@ -4,6 +4,7 @@ import repository.ElectronicDao;
 
 import java.sql.SQLException;
 import java.util.Collection;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -18,14 +19,21 @@ public class ElectronicService {
     }
 
     public void addNewOrderToList(String ItemName, int quantity) throws Exception {
-        Map<Integer, Integer> item = electronicDao.findItemIdPriceByName(ItemName);
-        int itemId = -1; //////////////////?????exception???
-        for (Integer key: item.keySet()) {
-            itemId = key;
+        int counter = 1;
+        if(counter <= 5){
+            Map<Integer, Integer> item = electronicDao.findItemIdPriceByName(ItemName);
+            int itemId = -1; //////////////////?????exception???
+            for (Integer key: item.keySet()) {
+                itemId = key;
+            }
+            int price = item.get(itemId);
+            int totalPrice = price * quantity;
+            electronicDao.saveNewItem(itemId, quantity, totalPrice);
+            counter++;
+        }else{
+            throw new Exception("Your shopping bag is full!\n1. Confirm\n2. remove some items");
         }
-        int price = item.get(itemId);
-        int totalPrice = price * quantity;
-        electronicDao.saveNewItem(itemId, quantity, totalPrice);
+
     }
 
 }
