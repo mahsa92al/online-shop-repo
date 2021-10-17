6d5d18aa0bb17d4ef833a76dfe07e4118b9e176a
Mahsa <mahsa.alikhany@gmail.com>
2021-10-17 16:36:28 +0330

    added saveNewCustomer to CustomerDao

--- src/repository/CustomerDao.java
+++ src/repository/CustomerDao.java
@@ -1,6 +1,9 @@
 package repository;
 
+import model.Customer;
+
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 /**
@@ -9,7 +12,18 @@ import java.sql.SQLException;
 public class CustomerDao extends BaseDao{
     private final Connection connection;
 
-    public CustomerDao(Connection connection) throws SQLException, ClassNotFoundException {
+    public CustomerDao() throws SQLException, ClassNotFoundException {
         this.connection = BaseDao.getConnection();
+    }
+    public void saveNewCustomer(Customer customer) throws SQLException {
+        String sqlQuery = "insert into customer name, email, password, phone, address_id" +
+                "values(?, ?, ?, ?, ?)";
+        PreparedStatement statement = connection.prepareStatement(sqlQuery);
+        statement.setString(1, customer.getName());
+        statement.setString(2, customer.getEmail());
+        statement.setString(3, customer.getPassword());
+        statement.setString(4, customer.getPhone());
+        statement.setInt(5, customer.getAddress().getId());
+        statement.executeUpdate();
     }
 }
