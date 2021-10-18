6e7dfe09cc594dc61a45fff4f9e3b88ad9a6c344
Mahsa <mahsa.alikhany@gmail.com>
2021-10-18 21:08:13 +0330

    removed findItemPriceByName and saveNewOrder from ElectrinicDao and added to ProductDao

--- src/repository/ProductDao.java
+++ src/repository/ProductDao.java
@@ -3,12 +3,11 @@ package repository;
 import model.Product;
 import model.enumeration.ProductCategory;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author Mahsa Alikhani m-58
@@ -34,4 +33,33 @@ public class ProductDao {
          }
          return products;
      }
+
+    public Map<Integer, Integer> findItemPriceByName(String name) throws Exception {
+        PreparedStatement statement = connection.prepareStatement(String.format("select id, price from products" +
+                "where name = '%s'", name) );
+        ResultSet resultSet = statement.executeQuery();
+        Map<Integer, Integer> map = new HashMap<>();
+        while (resultSet.next()){
+            if(resultSet == null){
+                throw new Exception("Item not found!");
+            }
+            int itemId = resultSet.getInt("id");
+            int price = resultSet.getInt("price");
+            map.put(itemId, price);
+            return map;
+        }
+        return null;
+    }
+
+    public void saveNewOrder(int productId, int quantity, int totalPrice, Date date) throws SQLException {
+        String sqlQuery = "insert into orders (product_id, quantity, total_price, date, customer_id)" +
+                "values(?, ?, ?, ?, ?)";
+        PreparedStatement statement = connection.prepareStatement(sqlQuery);
+        statement.setInt(1, productId);
+        statement.setInt(2, quantity);
+        statement.setInt(3, totalPrice);
+        statement.setDate(4, date);
+
+        statement.executeUpdate();
+    }
 }
