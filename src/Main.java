d3d4ed324689f6de00e344766d86d6d5bdb3897c
Mahsa <mahsa.alikhany@gmail.com>
2021-10-19 13:20:22 +0330

    added createAccountByCustomer to Main

--- src/Main.java
+++ src/Main.java
@@ -25,110 +25,111 @@ public class Main {
 
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
         customerService = new CustomerService();
-
-        boolean error = true;
-
-        System.out.println("***Welcome to online shop***");
-        System.out.println("1. Create Account");
-        String choice;
+        int choiceNumber;
         do{
-            choice = scanner.next();
-        }while (!choice.matches("[1-9]+"));
-        int choiceNumber = Integer.parseInt(choice);
-        switch (choiceNumber){
-            case 1:
-                String name = null;
-                do{
+            System.out.println("***Welcome to online shop***");
+            System.out.println("1. Create Account");
+            System.out.println("2. Exit");
+            String choice;
+            do{
+                choice = scanner.next();
+            }while (!choice.matches("[1-9]+"));
+            choiceNumber = Integer.parseInt(choice);
+            switch (choiceNumber){
+                case 1:
+                    int customerId = createAccountByCustomer();
+                    showProducts(productService.getAllProducts());
                     try {
-                        name = getNameFromCustomer();
-                        error = false;
-                    }catch (Exception e){
-                        System.out.println(e.getMessage());
-                    }
-                }while (error);
-                String email = null;
-                do{
-                    try {
-                        email = getEmailFromCustomer();
-                        error = false;
+                        addToShoppingBag(customerId);
                     } catch (Exception e) {
                         System.out.println(e.getMessage());;
                     }
-                }while (error);
-
-                String password = getPasswordFromCustomer();
-                String phone = null;
-                do{
-                    try {
-                        phone = getPhoneFromCustomer();
-                        error = false;
-                    } catch (Exception e) {
-                        System.out.println(e.getMessage());
+                    System.out.println("1. remove some items\n2. confirm ");
+                    do{
+                        choice = scanner.next();
+                    }while (!choice.matches("[1-9]+"));
+                    choiceNumber = Integer.parseInt(choice);
+                    switch (choiceNumber){
+                        case 1:
+                            List<Order> orderList = orderService.getOrderList(customerId);
+                            System.out.println(orderList);
+                            System.out.println("which order do you want to remove from bags?\nEnter order Id:");
+                            String orderId;
+                            do{
+                                orderId = scanner.next();
+                            }while (!orderId.matches("[1-9]+"));
+                            int orderIdNumber = Integer.parseInt(orderId);
+                            boolean remove = false;
+                            try {
+                                remove = orderService.removeOrderFromBag(orderIdNumber);
+                            } catch (Exception e) {
+                                System.out.println(e.getMessage());
+                            }
+                            if(remove){
+                                System.out.println("The order was removed successfully.");
+                            }else {
+                                System.out.println("The order was not removed.");
+                            }
+                            break;
+                        case 2:
+                            //TODO//confirm
+                            break;
                     }
-                }while (error);
-                Address address = null;
-                do{
-                    try {
-                        address = getAddressFromCustomer();
-                    } catch (Exception e) {
-                        System.out.println(e.getMessage());;
-                    }
-                }while (error);
-                customer = new Customer(name, email, password, phone, address);
-                int customerId = customerService.addNewCustomer(address, customer);
-                showProducts(productService.getAllProducts());
-                try {
-                    addToShoppingBag(customerId);
-                } catch (Exception e) {
-                    System.out.println(e.getMessage());;
-                }
-                System.out.println("1. Confirm\n2. remove some items");
-                do{
-                    choice = scanner.next();
-                }while (!choice.matches("[1-9]+"));
-                choiceNumber = Integer.parseInt(choice);
-                switch (choiceNumber){
-                    case 1:
-                        List<Order> orderList = orderService.getOrderList(customerId);
-                        System.out.println(orderList);
-                        System.out.println("which order do you want to remove from bags?\nEnter order Id:");
-                        String orderId;
-                        do{
-                            orderId = scanner.next();
-                        }while (!orderId.matches("[1-9]+"));
-                        int orderIdNumber = Integer.parseInt(orderId);
-                        boolean remove = false;
-                        try {
-                            remove = orderService.removeOrderFromBag(orderIdNumber);
-                        } catch (Exception e) {
-                            System.out.println(e.getMessage());
-                        }
-                        if(remove){
-                            System.out.println("The order was removed successfully.");
-                        }else {
-                            System.out.println("The order was not removed.");
-                        }
-                        break;
-                    case 2:
-                        //TODO
-                        break;
-                }
 
-                break;
-            case 2:
-                //TODO//show list
-                break;
-            case 3:
-                //TODO//show item prices
-                break;
-            case 4:
-                //TODO//exit
-                break;
-            default:
-                System.out.println("Invalid value!");
-        }
-
+                    break;
+                case 2:
+                    System.out.println("***Good Bye***");
+                    break;
+                default:
+                    System.out.println("Invalid value!");
+            }
+        }while(choiceNumber != 2);
     }
+
+    private static int createAccountByCustomer() throws SQLException {
+        boolean error = true;
+        String name = null;
+        do{
+            try {
+                name = getNameFromCustomer();
+                error = false;
+            }catch (Exception e){
+                System.out.println(e.getMessage());
+            }
+        }while (error);
+        String email = null;
+        do{
+            try {
+                email = getEmailFromCustomer();
+                error = false;
+            } catch (Exception e) {
+                System.out.println(e.getMessage());;
+            }
+        }while (error);
+
+        String password = getPasswordFromCustomer();
+        String phone = null;
+        do{
+            try {
+                phone = getPhoneFromCustomer();
+                error = false;
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }while (error);
+        Address address = null;
+        do{
+            try {
+                address = getAddressFromCustomer();
+            } catch (Exception e) {
+                System.out.println(e.getMessage());;
+            }
+        }while (error);
+        customer = new Customer(name, email, password, phone, address);
+        int customerId = customerService.addNewCustomer(address, customer);
+        return customerId;
+    }
+
     private static void addToShoppingBag(int customerId) throws Exception {
         System.out.println("Enter product ID:");
         String productId = scanner.next();
