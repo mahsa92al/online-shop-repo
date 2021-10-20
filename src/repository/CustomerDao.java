fc549e961a914708aca4f06b653b8a6c36e7b432
Mahsa <mahsa.alikhany@gmail.com>
2021-10-20 16:59:41 +0330

    fixed generatedKeys statement in saveNewCustomer in CustomerDao

--- src/repository/CustomerDao.java
+++ src/repository/CustomerDao.java
@@ -2,10 +2,7 @@ package repository;
 
 import model.Customer;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 
 /**
  * @author Mahsa Alikhani m-58
@@ -19,7 +16,7 @@ public class CustomerDao extends BaseDao{
     public int saveNewCustomer(Customer customer, int addressId) throws SQLException {
         String sqlQuery = "insert into customer (name, email, password, phone, address_id)" +
                 "values(?, ?, ?, ?, ?)";
-        PreparedStatement statement = connection.prepareStatement(sqlQuery);
+        PreparedStatement statement = connection.prepareStatement(sqlQuery, Statement.RETURN_GENERATED_KEYS);
         statement.setString(1, customer.getName());
         statement.setString(2, customer.getEmail());
         statement.setString(3, customer.getPassword());
